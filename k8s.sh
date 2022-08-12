2aa7961a3d5b39802c34c74b343efca57be9d186
Tridev <tridevguha17@gmail.com>
2022-08-12 22:46:05 +0530

    Create k8s.sh

---
+++ k8s.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+apt -y update
+sleep 5
+apt -y upgrade
+sleep 7
+apt install docker.io apt-transport-https curl -y
+sleep 5
+systemctl restart docker
+sleep 2
+systemctl enable docker
+sleep 2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
+sleep 2
+apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+sleep 3
+apt -y update
+sleep 2
+apt install kubeadm kubectl kubelet -y
+sleep 3
+apt-mark hold kubeadm kubectl kubelet
+sleep 2
+swapoff -a
+sleep 2
+free -mh
+
+
+# run on server only
+# kubeadm init --pod-network-cidr=10.244.0.0/16
+# export KUBECONFIG=/etc/kubernetes/admin.conf
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
