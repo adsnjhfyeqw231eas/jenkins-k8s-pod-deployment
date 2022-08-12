e85d9927e5b445bf82d33c027556defad1428cb6
Tridev <tridevguha17@gmail.com>
2022-08-12 22:27:35 +0530

    Create jenkins-setup.sh

---
+++ jenkins-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt update -y && apt install openjdk-11-jdk -y
+sleep 2
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt update -y 
+sleep 2
+apt install jenkins -y
+sleep 3
+which docker
+which jenkins
+which git
+cat /var/lib/jenkins/secrets/initialAdminPassword
