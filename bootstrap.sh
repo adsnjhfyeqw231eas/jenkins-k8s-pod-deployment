5e254b600e1d57089eb65c1d434dead7e807955a
Tridev <tridevguha17@gmail.com>
2022-08-27 13:07:44 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,7 @@ sudo apt update -y
 sleep 3
 sudo apt upgrade -y
 sleep 5
-sudo apt install git docker.io acl -y
-sleep 3
-sudo systemctl enable --now docker
-sleep 2
+sudo apt install git -y
+sleep 1
 sudo apt autoremove -y
 sleep 2
