3d5a8c9451e619dc7144f51d29179f19c3582e6c
Tridev <tridevguha17@gmail.com>
2023-04-13 12:41:11 +0530

    Updated Jenkins.sh for Year 2023 jenkins release

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update -y && apt install openjdk-11-jdk -y
 sleep 2
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | tee \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 apt update -y 
 sleep 2
